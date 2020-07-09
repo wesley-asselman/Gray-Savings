0ac3ef788fd4414305147c44b2765f1f7dfb0010
Wesley Asselman <wesley-asselman@hotmail.com>
2020-07-09 13:33:51 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php require "templates/header.php"; ?>
+<h1>Gray - Savings</h1>
---
+++ templates/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+    <html>
+        <head>
+            <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+            <!-- jQuery library -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+            <!-- Latest compiled JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+            <!-- Custom CSS -->
+            <link rel="stylesheet" href="css/style.css">
+        </head>
+    <body>
+<div class="container">
